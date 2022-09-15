3880a8039d813bfb2c2b46ebe676ee8d1a24b416
Ziba Kamali <kamali.zr97@gmail.com>
2022-09-15 00:11:18 -0700

    change onclick

---
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.git_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void findview(){
+
+
+    }
+    private void onclick(){
+        
+    }
+}
